d3348f9fc1afb779e87bee713cd21334461f9ebb
Bartnik Tomasz <Tomasz.Bartnik@asseco.pl>
2019-01-14 00:47:15 +0100

    F021 Dodano komponenty service, term, confirmation

--- reserve-client-app/src/app/app.module.ts
+++ reserve-client-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TermComponent } from './term/term.component';
+import { ServiceComponent } from './service/service.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TermComponent,
+    ServiceComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule
