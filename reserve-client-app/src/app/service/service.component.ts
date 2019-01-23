7139efd34f9b24f8138879b826842a1e3d297fb3
Kuba <dmuchu7@gmail.com>
2019-01-23 18:37:27 +0100

    Step progress bar mprovements

--- reserve-client-app/src/app/service/service.component.ts
+++ reserve-client-app/src/app/service/service.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceComponent implements OnInit {
 
+  strzyzenie = new Usluga('Strzyżenie męskie','20-30 min','20-35 zł');
+
   constructor() { }
 
   ngOnInit() {
   }
+}
+
+  class Usluga {
+    constructor(public name: string, public time: string,
+                public price: string){
+                }
 
 }
